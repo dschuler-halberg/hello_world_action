f989796ee8beb30f5258c3827eb6b477e8c91f3a
David Schuler <d.schuler@kbbz-halberg.de>
2021-03-05 10:31:14 +0100

    first commit

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hello
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Hallo Welt!");
+      Console.WriteLine("Test123");
+    }
+  }
+}
